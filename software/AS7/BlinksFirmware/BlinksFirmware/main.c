3a14519d0c85fb0923e9db450c83e14d389142a0
Josh Levine <github@junk.josh.com>
2017-03-26 23:26:11 -0400

    Pin down (pun) the RGB pin assignments

--- software/AS7/BlinksFirmware/BlinksFirmware/main.c
+++ software/AS7/BlinksFirmware/BlinksFirmware/main.c
@@ -11,6 +11,10 @@
 
 #include <util/delay.h>
 
+// Timer1 for internal time keeping (mostly timing IR pulses) because it is 16 bit and its pins happen to fall on ports that are handy for other stuff
+// Timer0 A=Red, B=Green. Both happen to be on handy pins
+// Timer2B for Blue duty. Works out perfectly because we can use OCR2A as a variable TOP to change the frequency for the charge pump, which is better to change than duty. 
+
 int main(void)
 {
     
